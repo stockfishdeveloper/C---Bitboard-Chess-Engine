a7f067312d10abf80f1f64fdb4643f8d2c79e4d3
stockfishdeveloper <joeblow@gmail.com>
2015-09-02 12:00:12 -0500

    Major Cleanup and Optimization
    Huge Changes

---
+++ Main.cpp
@@ -0,0 +1,16 @@
+using namespace std;
+#include <iostream>
+#include "magicmoves.c"//Pradu's Magic Bitboards
+#include "UCI.h"
+
+int main()
+{
+initmagicmoves();//Thanks, Pradu
+CheckUci();
+
+
+    return 0;
+
+}
+         
+
