32e51e4fa06c556a331c6455a15fa53dbf6e5f8e
stockfishdeveloper <joeblow@gmail.com>
2015-09-23 12:52:01 -0500

    Work on output to GUI
    Much more GUI friendly now; the engine will output infos like nodes and nps to the GUI

--- Thread.cpp
+++ Thread.cpp
@@ -1,8 +1,10 @@
 #include "tinythread.h"
 #include "Thread.h"
 #include "Search.h"
+#include <chrono>
 #include <iostream>
 #include <windows.h>
+
 using namespace std;
 using namespace tthread;
 
@@ -13,9 +15,19 @@ int f;
 // This is the child thread function
 void Runthread(void * aArg)
 {
+	typedef std::chrono::high_resolution_clock Time;
+    typedef std::chrono::milliseconds ms;
+    typedef std::chrono::duration<float> fsec;
+    auto t0 = Time::now();
+
   while(!Done_Searching)
   {
-      cout << "info nodes " << Nodes <<  endl;
-      Sleep(100);
+  	auto t1 = Time::now();
+  	fsec fs = t1 - t0;
+  	ms d = std::chrono::duration_cast<ms>(fs);    
+ 	float temporary = (Nodes / d.count());
+ 	
+    cout << "info time " << d.count() << " nodes " << Nodes << " nps " << (1000 *(Nodes / (d.count() + 1))) << endl;
+    Sleep(1000);
   } 
 }
