05eca3515254bb071a050c06c03fd1e91f3f4608
stockfishdeveloper <joeblow@gmail.com>
2015-09-23 12:40:58 -0500

    Fix the eval problem
    This was the problem with the alpha-beta search! Apparently, the compiler used external linkage on the White-Score and Black_Score vars in the header file. I had enough of the global variables, so I put them in the Evaluate_Position function and lo and behold, the search method worked! There are a few more problems with the search, and I will continue to work on it. This single commit has done the most good for this project!

--- Eval.cpp
+++ Eval.cpp
@@ -1,62 +1,63 @@
 using namespace std;
-#include "Bitboard.h"
 #include "Eval.h"
+#include "Bitboard.h"
 
-int White_Score = 0;
-int Black_Score = 0;
-
-int Evaluate_Position()
+int Evaluate_Position() 
 {
-	for(register int i = 0; i < 64; i++)
-	{
-		if(GeneralBoard[i] & White_Pawns)
-		White_Score += 1;
-	}
-	for(register int i = 0; i < 64; i++)
-	{
-		if(GeneralBoard[i] & White_Knights)
-		White_Score += 3;
-	}
-	for(register int i = 0; i < 64; i++)
-	{
-		if(GeneralBoard[i] & White_Bishops)
-		White_Score += 3;
-	}
-	for(register int i = 0; i < 64; i++)
-	{
-		if(GeneralBoard[i] & White_Rooks)
-		White_Score += 5;
-	}
-	for(register int i = 0; i < 64; i++)
-	{
-		if(GeneralBoard[i] & White_Queens)
-		White_Score += 8;
-	}
+	int White_Score = 0; 
+	int Black_Score = 0;
 	
-	for(register int i = 0; i < 64; i++)
-	{
-		if(GeneralBoard[i] & Black_Pawns)
-		Black_Score += 1;
-	}
-	for(register int i = 0; i < 64; i++)
-	{
-		if(GeneralBoard[i] & Black_Knights)
-		Black_Score += 3;
-	}
-	for(register int i = 0; i < 64; i++)
-	{
-		if(GeneralBoard[i] & Black_Bishops)
-		Black_Score += 3;
-	}
-	for(register int i = 0; i < 64; i++)
-	{
-		if(GeneralBoard[i] & Black_Rooks)
-		Black_Score += 5;
-	}
-	for(register int i = 0; i < 64; i++)
-	{
-		if(GeneralBoard[i] & Black_Queens)
-		Black_Score += 8;
-	}
-	return (White_Score - Black_Score);
+        register int i = 0;
+        for(i = 0; i < 64; i++)
+        {
+                if(GeneralBoard[i] & White_Pawns)
+                White_Score += 1;
+        }
+        for(i = 0; i < 64; i++)
+        {
+                if(GeneralBoard[i] & White_Knights)
+                White_Score += 3;
+        }
+        for(i = 0; i < 64; i++)
+        {
+                if(GeneralBoard[i] & White_Bishops)
+                White_Score += 3;
+        }
+        for(i = 0; i < 64; i++)
+        {
+                if(GeneralBoard[i] & White_Rooks)
+                White_Score += 5;
+        }
+        for(i = 0; i < 64; i++)
+        {
+                if(GeneralBoard[i] & White_Queens)
+                White_Score += 8;
+        }
+        
+        for(i = 0; i < 64; i++)
+        {
+                if(GeneralBoard[i] & Black_Pawns)
+                Black_Score += 1;
+        }
+        for(i = 0; i < 64; i++)
+        {
+                if(GeneralBoard[i] & Black_Knights)
+                Black_Score += 3;
+        }
+        for(i = 0; i < 64; i++)
+        {
+                if(GeneralBoard[i] & Black_Bishops)
+                Black_Score += 3;
+        }
+        for(i = 0; i < 64; i++)
+        {
+                if(GeneralBoard[i] & Black_Rooks)
+                Black_Score += 5;
+        }
+        for(int i = 0; i < 64; i++)
+        {
+                if(GeneralBoard[i] & Black_Queens)
+                Black_Score += 8;
+        }
+        return (White_Score - Black_Score);
 }
