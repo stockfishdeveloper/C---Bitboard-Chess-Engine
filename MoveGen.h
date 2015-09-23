a3cc8504cb705857c3370533f56a9d85db65dd3e
stockfishdeveloper <joeblow@gmail.com>
2015-09-23 12:47:06 -0500

    Add some kingside castling capability
    Just a couple of variable declarations

--- MoveGen.h
+++ MoveGen.h
@@ -66,5 +66,7 @@ int Stack_Black_Queen_Moves();
 
 extern int White_Move_Spacer; // Keeps a "record" of the last move put on the stack so that it knows which index of the array to put the next move in
 extern int Black_Move_Spacer;
+extern bool WhiteHasCastled;
+extern bool BlackHasCastled;
 
 #endif
