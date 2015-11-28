d80b1b3e46648e3114b196656474090ebd6adc8f
stockfishdeveloper <joeblow@gmail.com>
2015-11-28 08:40:55 -0600

    Code Cleanup
    Format the code.

--- MakeMove.cpp
+++ MakeMove.cpp
@@ -6,43 +6,15 @@
 		int MakeWhiteMove(Move Best_Move)
 {
 	
-  //The following few lines simply generate a random number between 0 and White or Black MoveSpacer and put this value into the variable q
-	/*	  srand(static_cast<unsigned int>(time(0)));
-		int q;
-        int randomNumber = rand();
-		if(White_Move_Spacer > 1)//We must choose a number smaller than the number of legal moves found
-        q = (randomNumber % (White_Move_Spacer - 1));
-        
-        if(White_Move_Spacer == 1)//If there is only one move, then force the program to play it
-        q = 0; 
-        if(White_Move_Spacer == 0)//If q = 0, then white loses
-         return 0; //To let the user know that the game finished
-        for( int h = 0; h < 64; h++)
-        {
-        	if(GeneralBoard[h] & White_Move_From_Stack[q])//Get the index in GeneralBoard[] of the square the current pieces is moving from
-        	{
-        	cout << "bestmove " << PlayerMoves[h];//Cout it to the user, but don't endl because we have to display the whole move squashed togather, e.g. d2d4
-        	//Log << "bestmove " << PlayerMoves[h];//Put it out to the text file
-        }
-		}
-		for( int h = 0; h < 64; h++)
-        {
-        	if(GeneralBoard[h] & White_Move_To_Stack[q])//For each to square found
-        	{
-        	cout << PlayerMoves[h] << endl;//cout it and
-        	//Log << PlayerMoves[h] << endl;//Put it into the text file; now we can do a new line because we are done with the current move
-        }
-		}	
-		*/	
-               // Update Bitboards
+  // Update Bitboards
   Bitboard From = Best_Move.From;             
   Bitboard To = Best_Move.To;
   int Move_Type = Best_Move.Move_Type;
 
-	switch(Move_Type)//This switch evaluates the type of move that accompanies the index of the move stack that q refers to
-               { 
+switch(Move_Type)//This switch evaluates the type of move that accompanies the index of the move stack that q refers to
+    { 
                
-               case 1://A (white) pawn capture
+                case 1://A (white) pawn capture
                	White_Pieces |= To;//Move white's pieces to the to square and from the from square
                	White_Pieces ^= From;
                	White_Pawns |= To;
@@ -234,18 +206,17 @@
                	White_King |= To;
                	White_King ^= From;
                	WhiteHasCastled = true;
-               	break;*/
-				   	
+               	break;*/				   	
                	
 			   }
                  
                  //Tidy up for the next call of the move generation functions
-				 for(int t = 0; t < White_Move_Spacer; t++)               
-               {
-               	 White_Move_From_Stack[t] = 0;//Clear the move from stack
-               	 White_Move_To_Stack[t] = 0;//Clear the move to stack
-               	 White_Move_Types[t] = 0;//Clear the move types associated with the moves
-			   }
+				for(int t = 0; t < White_Move_Spacer; t++)               
+               		{
+               	 		White_Move_From_Stack[t] = 0;//Clear the move from stack
+               	 		White_Move_To_Stack[t] = 0;//Clear the move to stack
+               	 		White_Move_Types[t] = 0;//Clear the move types associated with the moves
+			   		}
 			    White_Knight_Spacer = 0;//Clear all of the piece spacers
 			    White_King_Spacer = 0;
 			    White_Pawn_Spacer = 0;
@@ -266,33 +237,7 @@
 		//Pretty much the same as White's MakeMove()
 		int MakeBlackMove(Move Best_Move)
 		{	
-		  /*srand(static_cast<unsigned int>(time(0)));
-
-        int randomNumber = rand();
-		int q;
-		if(Black_Move_Spacer > 1)
-        q = (randomNumber % (Black_Move_Spacer - 1));
-        if(Black_Move_Spacer == 1)
-        q = 0;
-        if(Black_Move_Spacer == 0)
-        cin >> q;
-        for(int y = 0; y < 64; y++)
-        {
-        	if(GeneralBoard[y] & Black_Move_From_Stack[q])
-        	{
-        	cout << "bestmove " << PlayerMoves[y]; 
-        	//Log << "bestmove " << PlayerMoves[h];
-        }
-		}
-		for(int h = 0; h < 64; h++)
-        {
-        	if(GeneralBoard[h] & Black_Move_To_Stack[q])
-        	{
-        	cout << PlayerMoves[h] << endl; 
-        	//Log << PlayerMoves[h] << endl;
-        }
-		}	    
-		*/  
+		  
 		Bitboard From = Best_Move.From;            
   		Bitboard To = Best_Move.To;
   		int Move_Type = Best_Move.Move_Type;
