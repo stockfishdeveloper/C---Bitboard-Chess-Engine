0e1b7b36fa1026a21921a15978865476d687156c
stockfishdeveloper <joeblow@gmail.com>
2015-12-19 07:50:55 -0600

    Delete MakeMove.h

--- MakeMove.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAKE_MOVE_H_INCLUDED
-#define MAKE_MOVE_H_INCLUDED
-#include <string>
-#include "Search.h"
-int MakeWhiteMove(Move Best_Move);
-int MakeBlackMove(Move Best_Move);
-
-#endif
-
