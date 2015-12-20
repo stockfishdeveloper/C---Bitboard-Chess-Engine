4cd51bc2a7a484d6a7bdf04937fbdfddda475ba0
stockfishdeveloper <joeblow@gmail.com>
2015-12-20 15:01:51 -0600

    Make namespace Eval

--- Eval.h
+++ Eval.h
@@ -1,5 +1,11 @@
 #ifndef _Eval_H_Included_
 #define _Eval_H_Included_
-
+namespace Eval
+{
 int Evaluate_Position();
+extern int WNpsqt[64];
+extern int BNpsqt[64];
+extern int WPpsqt[64];
+extern int BPpsqt[64];
+}
 #endif
