bd88d5688c4c87f86d92d4f5cd54be818a70f6f4
stockfishdeveloper <joeblow@gmail.com>
2016-01-19 14:55:38 -0600

    Make move stacks bigger
    To help in debugging FEN and perft; it had to be done sometime anyway!

--- MoveGen.h
+++ MoveGen.h
@@ -1,6 +1,7 @@
 #ifndef Make_Move_H_Included
 #define Make_Move_H_Included
 #include "Bitboard.h"
+#include <iostream>
 extern bool Current_Turn;
 extern bool White_Turn;
 void Generate_White_Moves();
@@ -8,12 +9,12 @@ void Generate_Black_Moves();
 bool White_Is_Legal(const Bitboard& From_Square, const Bitboard& To_Square, const int Move_Type);//Tests for legality
 bool Black_Is_Legal(const Bitboard& From_Square, const Bitboard& To_Square, const int Move_Type);//Tests for legality
 extern bool Is_Legal;
-extern Bitboard White_Move_From_Stack[70];//Move stack is just an array of Bitboards(64-bit integers) containing only one bit set in each--the from square or the to square
-extern Bitboard White_Move_To_Stack[70];
-extern Bitboard Black_Move_From_Stack[70];
-extern Bitboard Black_Move_To_Stack[70];
-extern int White_Move_Types[70];//Array of normal integers that keeps track of the type of each move
-extern int Black_Move_Types[70];
+extern Bitboard White_Move_From_Stack[150];//Move stack is just an array of Bitboards(64-bit integers) containing only one bit set in each--the from square or the to square
+extern Bitboard White_Move_To_Stack[150];
+extern Bitboard Black_Move_From_Stack[150];
+extern Bitboard Black_Move_To_Stack[150];
+extern int White_Move_Types[150];//Array of normal integers that keeps track of the type of each move
+extern int Black_Move_Types[150];
 
 extern Bitboard WhiteKingCount[8];//Array which is large enough to contain as many legal moves at a time for all of the pieces of that type as possible
 extern Bitboard BlackKingCount[8];
@@ -21,12 +22,12 @@ extern Bitboard WhiteKnightCount[40];
 extern Bitboard BlackKnightCount[40];
 extern Bitboard WhitePawnCount[20];
 extern Bitboard BlackPawnCount[20];
-extern Bitboard WhiteRookCount[20];
-extern Bitboard BlackRookCount[20];
+extern Bitboard WhiteRookCount[60];
+extern Bitboard BlackRookCount[60];
 extern Bitboard WhiteBishopCount[20];
 extern Bitboard BlackBishopCount[20];
-extern Bitboard WhiteQueenCount[28];
-extern Bitboard BlackQueenCount[28];
+extern Bitboard WhiteQueenCount[150];
+extern Bitboard BlackQueenCount[150];
 
 extern int White_Knight_Spacer;//Keeps track of the number of psuedo legal moves for each type of piece
 extern int Black_Knight_Spacer;
