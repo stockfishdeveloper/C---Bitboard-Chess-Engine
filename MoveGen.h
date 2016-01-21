aa771cd2724bbd7d9daaa5f895e236fb0656c4b7
stockfishdeveloper <joeblow@gmail.com>
2016-01-21 11:27:49 -0600

    Make move stacks smaller
    So that I do not go over the 4 MB maximum on the stack

--- MoveGen.h
+++ MoveGen.h
@@ -9,12 +9,12 @@ void Generate_Black_Moves();
 bool White_Is_Legal(const Bitboard& From_Square, const Bitboard& To_Square, const int Move_Type);//Tests for legality
 bool Black_Is_Legal(const Bitboard& From_Square, const Bitboard& To_Square, const int Move_Type);//Tests for legality
 extern bool Is_Legal;
-extern Bitboard White_Move_From_Stack[150];//Move stack is just an array of Bitboards(64-bit integers) containing only one bit set in each--the from square or the to square
-extern Bitboard White_Move_To_Stack[150];
-extern Bitboard Black_Move_From_Stack[150];
-extern Bitboard Black_Move_To_Stack[150];
-extern int White_Move_Types[150];//Array of normal integers that keeps track of the type of each move
-extern int Black_Move_Types[150];
+extern Bitboard White_Move_From_Stack[100];//Move stack is just an array of Bitboards(64-bit integers) containing only one bit set in each--the from square or the to square
+extern Bitboard White_Move_To_Stack[100];
+extern Bitboard Black_Move_From_Stack[100];
+extern Bitboard Black_Move_To_Stack[100];
+extern int White_Move_Types[100];//Array of normal integers that keeps track of the type of each move
+extern int Black_Move_Types[100];
 
 extern Bitboard WhiteKingCount[8];//Array which is large enough to contain as many legal moves at a time for all of the pieces of that type as possible
 extern Bitboard BlackKingCount[8];
@@ -26,8 +26,8 @@ extern Bitboard WhiteRookCount[60];
 extern Bitboard BlackRookCount[60];
 extern Bitboard WhiteBishopCount[20];
 extern Bitboard BlackBishopCount[20];
-extern Bitboard WhiteQueenCount[150];
-extern Bitboard BlackQueenCount[150];
+extern Bitboard WhiteQueenCount[100];
+extern Bitboard BlackQueenCount[100];
 
 extern int White_Knight_Spacer;//Keeps track of the number of psuedo legal moves for each type of piece
 extern int Black_Knight_Spacer;
