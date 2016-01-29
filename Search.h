3074c7fa5f819421b5a19228f15c6457a3716307
stockfishdeveloper <joeblow@gmail.com>
2016-01-29 11:08:36 -0600

    Chess 1.0

--- Search.h
+++ Search.h
@@ -150,8 +150,8 @@ class Timer
 namespace Search
 { 
 Move Think(int wtime, int btime, int winc, int binc);
-Move SearchMax(Move alpha, Move beta, int depth, LINE * pline);
-Move SearchMin(Move alpha, Move beta, int depth, LINE * pline);
+int SearchMax(int alpha, int beta, int depth, LINE * pline);
+int SearchMin(int alpha, int beta, int depth, LINE * pline);
 int Make_White_Search_Move(const Bitboard& From, const Bitboard& To, const int Move_Type);
 int Make_Black_Search_Move(const Bitboard& From, const Bitboard& To, const int Move_Type);
 //extern bool Fake_Current_Turn;
