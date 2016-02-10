4d34d496d17e4c0f159a8bd77a485bfe5de8bf8b
stockfishdeveloper <joeblow@gmail.com>
2016-02-10 10:20:28 -0600

    Add king piece square tables

--- Eval.h
+++ Eval.h
@@ -2,10 +2,12 @@
 #define _Eval_H_Included_
 namespace Eval
 {
-int Evaluate_Position();
-extern int WNpsqt[64];
-extern int BNpsqt[64];
-extern int WPpsqt[64];
-extern int BPpsqt[64];
+const int Evaluate_Position();
+extern const int WNpsqt[64];
+extern const int BNpsqt[64];
+extern const int WPpsqt[64];
+extern const int BPpsqt[64];
+extern const int WKpsqt[64];
+extern const int BKpsqt[64];
 }
 #endif
