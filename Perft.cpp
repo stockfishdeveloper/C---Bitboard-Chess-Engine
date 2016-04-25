0075ca29b4789b3536e10e8cb628f421dfb96221
stockfishdeveloper <joeblow@gmail.com>
2016-04-25 11:23:14 -0500

    Speed up perft
    Pretty much doubles perft speed. Still pretty slow though!

--- Perft.cpp
+++ Perft.cpp
@@ -7,7 +7,7 @@ int Root_Perft(int depth)
     Bitboard nodes = 0;
 
     if (depth == 0) return 1;
-    Search::Current_Turn ? Generate_White_Moves() : Generate_Black_Moves();
+    Search::Current_Turn ? Generate_White_Moves(false) : Generate_Black_Moves(false);
     n_moves = Search::Current_Turn ? White_Move_Spacer : Black_Move_Spacer;
     Move move;
     for (i = 0; i < n_moves; i++)
@@ -18,24 +18,6 @@ int Root_Perft(int depth)
         Search::Current_Turn ? move.Move_Type = White_Move_Types[i] : move.Move_Type = Black_Move_Types[i];
         Search::Current_Turn ? move.White_Temp_Move_Spacer = White_Move_Spacer : move.Black_Temp_Move_Spacer = Black_Move_Spacer;
 
-        if(Search::Current_Turn)
-        {
-            for(int h = 0; h < White_Move_Spacer; h++)
-            {
-                move.White_Temp_Move_From_Stack[h] = move.Convert_Bitboard(White_Move_From_Stack[h]);
-                move.White_Temp_Move_To_Stack[h] = move.Convert_Bitboard(White_Move_To_Stack[h]);
-                move.White_Temp_Move_Types[h] = White_Move_Types[h];
-            }
-        }
-        else
-        {
-            for(int h = 0; h < Black_Move_Spacer; h++)
-            {
-                move.Black_Temp_Move_From_Stack[h] = move.Convert_Bitboard(Black_Move_From_Stack[h]);
-                move.Black_Temp_Move_To_Stack[h] = move.Convert_Bitboard(Black_Move_To_Stack[h]);
-                move.Black_Temp_Move_Types[h] = Black_Move_Types[h];
-            }
-        }
         Search::Current_Turn ? Search::Make_White_Search_Move(White_Move_From_Stack[i], White_Move_To_Stack[i], White_Move_Types[i]) : Search::Make_Black_Search_Move(Black_Move_From_Stack[i], Black_Move_To_Stack[i], Black_Move_Types[i]);
         for( int h = 0; h < 64; h++)
         {
@@ -65,7 +47,7 @@ int Perft(int depth)
     Bitboard nodes = 0;
 
     if (depth == 0) return 1;
-    Search::Current_Turn ? Generate_White_Moves() : Generate_Black_Moves();
+    Search::Current_Turn ? Generate_White_Moves(false) : Generate_Black_Moves(false);
     n_moves = Search::Current_Turn ? White_Move_Spacer : Black_Move_Spacer;
     //cout << "n_moves: " << n_moves << endl;
     for (i = 0; i < n_moves; i++)
@@ -75,24 +57,7 @@ int Perft(int depth)
         Search::Current_Turn ? move.To = White_Move_To_Stack[i] : move.To = Black_Move_To_Stack[i];
         Search::Current_Turn ? move.Move_Type = White_Move_Types[i] : move.Move_Type = Black_Move_Types[i];
         Search::Current_Turn ? move.White_Temp_Move_Spacer = White_Move_Spacer : move.Black_Temp_Move_Spacer = Black_Move_Spacer;
-        if(Search::Current_Turn)
-        {
-            for(int h = 0; h < White_Move_Spacer; h++)
-            {
-                move.White_Temp_Move_From_Stack[h] = move.Convert_Bitboard(White_Move_From_Stack[h]);
-                move.White_Temp_Move_To_Stack[h] = move.Convert_Bitboard(White_Move_To_Stack[h]);
-                move.White_Temp_Move_Types[h] = White_Move_Types[h];
-            }
-        }
-        else
-        {
-            for(int h = 0; h < Black_Move_Spacer; h++)
-            {
-                move.Black_Temp_Move_From_Stack[h] = move.Convert_Bitboard(Black_Move_From_Stack[h]);
-                move.Black_Temp_Move_To_Stack[h] = move.Convert_Bitboard(Black_Move_To_Stack[h]);
-                move.Black_Temp_Move_Types[h] = Black_Move_Types[h];
-            }
-        }
+        
         Search::Current_Turn ? Search::Make_White_Search_Move(White_Move_From_Stack[i], White_Move_To_Stack[i], White_Move_Types[i]) : Search::Make_Black_Search_Move(Black_Move_From_Stack[i], Black_Move_To_Stack[i], Black_Move_Types[i]);
         nodes += Perft(depth - 1);
         move.Undo_Move();
