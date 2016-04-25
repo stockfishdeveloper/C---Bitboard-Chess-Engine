8fa14b177c4c534cb14a48b67506aebd6cb8cf2e
stockfishdeveloper <joeblow@gmail.com>
2016-04-25 11:19:40 -0500

    Edit startup tasks
    A couple of new functions need to be called at startup

--- Main.cpp
+++ Main.cpp
@@ -1,14 +1,20 @@
 using namespace std;
+#include "TransTable.h"
 #include <iostream>
 #include "magicmoves.c"//Pradu's Magic Bitboards
 #include "UCI.h"
 
 int main()
-	{
-		initmagicmoves();//Thanks, Pradu 
-		cout << Engine_Info();
-		cout << "\nA chess engine by David Cimbalista\nCopyright (c) 2015" << endl;
-		CheckUci(); //Main command loop
-    	return 0;
-	}
-         
+{
+    initmagicmoves();//Thanks, Pradu
+    cout << Engine_Info();
+    cout << "\nA chess engine by David Cimbalista\nCopyright (c) 2015" << endl;
+    //Init_Zobrist();
+    #include "Nalimov\TBINDEX.h"
+    char* f = "c:\\Tablebases";
+    //cout << IInitializeTb(f) << endl;
+    CheckUci(); //Main command loop
+    return 0;
+}
+
+
