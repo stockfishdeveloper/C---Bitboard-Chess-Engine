96e8bcd4b4908928a8ba599dcee2ec3dc4dbd748
stockfishdeveloper <joeblow@gmail.com>
2016-04-25 11:15:30 -0500

    Add zobrist key
    Add zobrist key for the transposition table

--- Bitboard.h
+++ Bitboard.h
@@ -6,7 +6,7 @@ typedef unsigned long long int Bitboard;
 using namespace std;
 //This is an array containing one bit set in each bitboard corresponding to the index of the array; the square A1 is the first (0th) element
 extern Bitboard GeneralBoard[64];
-    
+
 extern Bitboard Eigth_Rank_White;//For masking out pawn pushes to the eagth rank for each side--white and black
 extern Bitboard Eigth_Rank_Black;//See last comment
 extern Bitboard Seventh_Rank_White;//For masking out pawn pushes to the eagth rank for each side--white and black
@@ -26,6 +26,7 @@ extern Bitboard Black_Bishops;
 extern Bitboard Black_Knights;
 extern Bitboard Black_Pawns;
 
+extern Bitboard CurrZobkey;
 //Contains the squares that a knight on a given index can move to. For example, the 0th element has the bits of b3 and c2 set
 extern Bitboard Knight_Lookup_Table[64];
 
@@ -34,18 +35,19 @@ extern Bitboard Knight_Lookup_Table[64];
 //This one is the same as the Knight_Lookup_Table except that this one is for kings
 extern Bitboard King_Lookup_Table[64];
 
- 
- extern Bitboard H_Pawn_Mask;//For masking out A and H pawns so that they cannot capture off of the end of the board
- extern Bitboard A_Pawn_Mask;
- extern Bitboard G_Pawn_Mask;
- extern Bitboard B_Pawn_Mask;
- extern Bitboard F_Pawn_Mask;
- extern Bitboard C_Pawn_Mask;
- extern Bitboard E_Pawn_Mask;
- extern Bitboard D_Pawn_Mask;
+
+extern Bitboard H_Pawn_Mask;//For masking out A and H pawns so that they cannot capture off of the end of the board
+extern Bitboard A_Pawn_Mask;
+extern Bitboard G_Pawn_Mask;
+extern Bitboard B_Pawn_Mask;
+extern Bitboard F_Pawn_Mask;
+extern Bitboard C_Pawn_Mask;
+extern Bitboard E_Pawn_Mask;
+extern Bitboard D_Pawn_Mask;
 
 //Contains a string representation of the squares of the chess board; for example, the 0th element in the array is "a1"
 extern string PlayerMoves[64];
 void Print_Board();
 
 #endif//Bitboard.h
+
