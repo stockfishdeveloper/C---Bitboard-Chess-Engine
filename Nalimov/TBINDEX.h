9e22ce955bb1884a803c470dc9b8be07f704830e
stockfishdeveloper <joeblow@gmail.com>
2016-04-26 10:31:50 -0500

    Headers for Nalimov tablebases

---
+++ Nalimov/TBINDEX.h
@@ -0,0 +1,10 @@
+#ifndef TBINDEX_H_
+#define TBINDEX_H_
+#define NEW
+#define T41_INCLUDE
+#define T33_INCLUDE
+int IInitializeTb(char *pszPath);
+int IDescFindFromCounters(int *); 
+int PfnIndCalcFun(int iTb, int side); 
+int TbtProbeTable(int iTb, int side, unsigned long indOffset);
+#endif
