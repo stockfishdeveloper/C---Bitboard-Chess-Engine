fd51a988c560c7d61f6d74b862773e92ddceaeac
stockfishdeveloper <joeblow@gmail.com>
2016-04-27 11:16:59 -0500

    Add rook on open file bonus
    Good for about 40-90 ELO

--- Eval.cpp
+++ Eval.cpp
@@ -108,6 +108,7 @@ int White_Score = 0; //White's Score
 int Black_Score = 0; //Black's Score
 int wk = 0, bk = 0;
 const int Doubled_Pawn_Penalty = 10;
+const int Open_File_Bonus = 10;
 //********************************************************************
 //Material
 //********************************************************************
@@ -149,7 +150,7 @@ const int Eval::Evaluate_Position()
 			    White_Score += WBpsqt[i];
 			continue;    
         }
-    	else if(GeneralBoard[i] & Black_Pieces)
+    	else
     		{
         		if(GeneralBoard[i] & Black_Pawns) //For each black pawn on the board
         			Black_Score += BPpsqt[i];
@@ -164,23 +165,100 @@ const int Eval::Evaluate_Position()
         
     	}
     
-    White_Score -= (Doubled_Pawn_Penalty * (__builtin_popcountll(White_Pawns & A_Pawn_Mask) - 1));
-    White_Score -= (Doubled_Pawn_Penalty * (__builtin_popcountll(White_Pawns & B_Pawn_Mask) - 1));
-    White_Score -= (Doubled_Pawn_Penalty * (__builtin_popcountll(White_Pawns & C_Pawn_Mask) - 1));
-    White_Score -= (Doubled_Pawn_Penalty * (__builtin_popcountll(White_Pawns & D_Pawn_Mask) - 1));
-    White_Score -= (Doubled_Pawn_Penalty * (__builtin_popcountll(White_Pawns & E_Pawn_Mask) - 1));
-    White_Score -= (Doubled_Pawn_Penalty * (__builtin_popcountll(White_Pawns & F_Pawn_Mask) - 1));
-    White_Score -= (Doubled_Pawn_Penalty * (__builtin_popcountll(White_Pawns & G_Pawn_Mask) - 1));
-    White_Score -= (Doubled_Pawn_Penalty * (__builtin_popcountll(White_Pawns & H_Pawn_Mask) - 1));
+    int wpona = __builtin_popcountll(White_Pawns & A_Pawn_Mask);
+    int wponb = __builtin_popcountll(White_Pawns & B_Pawn_Mask);
+    int wponc = __builtin_popcountll(White_Pawns & C_Pawn_Mask);
+    int wpond = __builtin_popcountll(White_Pawns & D_Pawn_Mask);
+    int wpone = __builtin_popcountll(White_Pawns & E_Pawn_Mask);
+    int wponf = __builtin_popcountll(White_Pawns & F_Pawn_Mask);
+    int wpong = __builtin_popcountll(White_Pawns & G_Pawn_Mask);
+    int wponh = __builtin_popcountll(White_Pawns & H_Pawn_Mask);
     
-    Black_Score -= (Doubled_Pawn_Penalty * (__builtin_popcountll(Black_Pawns & A_Pawn_Mask) - 1));
-    Black_Score -= (Doubled_Pawn_Penalty * (__builtin_popcountll(Black_Pawns & B_Pawn_Mask) - 1));
-    Black_Score -= (Doubled_Pawn_Penalty * (__builtin_popcountll(Black_Pawns & C_Pawn_Mask) - 1));
-    Black_Score -= (Doubled_Pawn_Penalty * (__builtin_popcountll(Black_Pawns & D_Pawn_Mask) - 1));
-    Black_Score -= (Doubled_Pawn_Penalty * (__builtin_popcountll(Black_Pawns & E_Pawn_Mask) - 1));
-    Black_Score -= (Doubled_Pawn_Penalty * (__builtin_popcountll(Black_Pawns & F_Pawn_Mask) - 1));
-    Black_Score -= (Doubled_Pawn_Penalty * (__builtin_popcountll(Black_Pawns & G_Pawn_Mask) - 1));
-    Black_Score -= (Doubled_Pawn_Penalty * (__builtin_popcountll(Black_Pawns & H_Pawn_Mask) - 1));
+    White_Score -= (wpona > 1 ? Doubled_Pawn_Penalty * (wpona - 1) : 0);
+    White_Score -= (wponb > 1 ? Doubled_Pawn_Penalty * (wponb - 1) : 0);
+    White_Score -= (wponc > 1 ? Doubled_Pawn_Penalty * (wponc - 1) : 0);
+    White_Score -= (wpond > 1 ? Doubled_Pawn_Penalty * (wpond - 1) : 0);
+    White_Score -= (wpone > 1 ? Doubled_Pawn_Penalty * (wpone - 1) : 0);
+    White_Score -= (wponf > 1 ? Doubled_Pawn_Penalty * (wponf - 1) : 0);
+    White_Score -= (wpong > 1 ? Doubled_Pawn_Penalty * (wpong - 1) : 0);
+    White_Score -= (wponh > 1 ? Doubled_Pawn_Penalty * (wponh - 1) : 0);
+    
+    int bpona = (__builtin_popcountll(Black_Pawns & A_Pawn_Mask) - 1);
+    int bponb = (__builtin_popcountll(Black_Pawns & B_Pawn_Mask) - 1);
+    int bponc = (__builtin_popcountll(Black_Pawns & C_Pawn_Mask) - 1);
+    int bpond = (__builtin_popcountll(Black_Pawns & D_Pawn_Mask) - 1);
+    int bpone = (__builtin_popcountll(Black_Pawns & E_Pawn_Mask) - 1);
+    int bponf = (__builtin_popcountll(Black_Pawns & F_Pawn_Mask) - 1);
+    int bpong = (__builtin_popcountll(Black_Pawns & G_Pawn_Mask) - 1);
+    int bponh = (__builtin_popcountll(Black_Pawns & H_Pawn_Mask) - 1);
+    
+    Black_Score -= (bpona > 1 ? Doubled_Pawn_Penalty * (bpona - 1) : 0);
+    Black_Score -= (bponb > 1 ? Doubled_Pawn_Penalty * (bponb - 1) : 0);
+    Black_Score -= (bponc > 1 ? Doubled_Pawn_Penalty * (bponc - 1) : 0);
+    Black_Score -= (bpond > 1 ? Doubled_Pawn_Penalty * (bpond - 1) : 0);
+    Black_Score -= (bpone > 1 ? Doubled_Pawn_Penalty * (bpone - 1) : 0);
+    Black_Score -= (bponf > 1 ? Doubled_Pawn_Penalty * (bponf - 1) : 0);
+    Black_Score -= (bpong > 1 ? Doubled_Pawn_Penalty * (bpong - 1) : 0);
+    Black_Score -= (bponh > 1 ? Doubled_Pawn_Penalty * (bponh - 1) : 0);
+    
+    if((wpona + bpona) < 2) 
+    {
+		if(White_Rooks & A_Pawn_Mask)
+			White_Score += Open_File_Bonus;
+		if(Black_Rooks & A_Pawn_Mask)
+			Black_Score += Open_File_Bonus;
+	}
+	if((wponb + bponb) < 2) 
+    {
+		if(White_Rooks & B_Pawn_Mask)
+			White_Score += Open_File_Bonus;
+		if(Black_Rooks & B_Pawn_Mask)
+			Black_Score += Open_File_Bonus;
+	}
+	if((wponc + bponc) < 2) 
+    {
+		if(White_Rooks & C_Pawn_Mask)
+			White_Score += Open_File_Bonus;
+		if(Black_Rooks & C_Pawn_Mask)
+			Black_Score += Open_File_Bonus;
+	}
+	if((wpond + bpond) < 2) 
+    {
+		if(White_Rooks & D_Pawn_Mask)
+			White_Score += Open_File_Bonus;
+		if(Black_Rooks & D_Pawn_Mask)
+			Black_Score += Open_File_Bonus;
+	}
+	if((wpone + bpone) < 2) 
+    {
+		if(White_Rooks & E_Pawn_Mask)
+			White_Score += Open_File_Bonus;
+		if(Black_Rooks & E_Pawn_Mask)
+			Black_Score += Open_File_Bonus;
+	}
+	if((wponf + bponf) < 2) 
+    {
+		if(White_Rooks & F_Pawn_Mask)
+			White_Score += Open_File_Bonus;
+		if(Black_Rooks & F_Pawn_Mask)
+			Black_Score += Open_File_Bonus;
+	}
+	if((wpong + bpong) < 2) 
+    {
+		if(White_Rooks & G_Pawn_Mask)
+			White_Score += Open_File_Bonus;
+		if(Black_Rooks & G_Pawn_Mask)
+			Black_Score += Open_File_Bonus;
+	}
+	if((wponh + bponh) < 2) 
+    {
+		if(White_Rooks & H_Pawn_Mask)
+			White_Score += Open_File_Bonus;
+		if(Black_Rooks & H_Pawn_Mask)
+			Black_Score += Open_File_Bonus;
+	}
+    
+    
     if(__builtin_popcountll(White_Pieces | Black_Pieces) <= 10)
                 {
                 	if(White_Score > Black_Score)
