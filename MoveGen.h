ebed5e54d37d7fb6cffbdfa7f441d993a33976b9
stockfishdeveloper <joeblow@gmail.com>
2016-05-02 09:31:51 -0500

    Revamp move generation
    Cut the size of movegen in half and double the speed

--- MoveGen.h
+++ MoveGen.h
@@ -15,31 +15,6 @@ extern Bitboard Black_Move_To_Stack[100];
 extern int White_Move_Types[100];//Array of normal integers that keeps track of the type of each move
 extern int Black_Move_Types[100];
 
-extern Bitboard WhiteKingCount[8];//Array which is large enough to contain as many legal moves at a time for all of the pieces of that type as possible
-extern Bitboard BlackKingCount[8];
-extern Bitboard WhiteKnightCount[40];
-extern Bitboard BlackKnightCount[40];
-extern Bitboard WhitePawnCount[20];
-extern Bitboard BlackPawnCount[20];
-extern Bitboard WhiteRookCount[60];
-extern Bitboard BlackRookCount[60];
-extern Bitboard WhiteBishopCount[20];
-extern Bitboard BlackBishopCount[20];
-extern Bitboard WhiteQueenCount[100];
-extern Bitboard BlackQueenCount[100];
-
-extern int White_Knight_Spacer;//Keeps track of the number of psuedo legal moves for each type of piece
-extern int Black_Knight_Spacer;
-extern int White_King_Spacer;
-extern int Black_King_Spacer;
-extern int White_Pawn_Spacer;
-extern int Black_Pawn_Spacer;
-extern int White_Rook_Spacer;
-extern int Black_Rook_Spacer;
-extern int White_Bishop_Spacer;
-extern int Black_Bishop_Spacer;
-extern int White_Queen_Spacer;
-extern int Black_Queen_Spacer;
 extern int White_Move_Spacer; // Keeps a "record" of the last move put on the stack so that it knows which index of the array to put the next move in
 extern int Black_Move_Spacer;
 extern bool WhiteCanCastleK;
