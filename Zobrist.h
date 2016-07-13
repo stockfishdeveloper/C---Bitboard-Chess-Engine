d22a404d4bf5686ddae4b6ccef5c719e36228d3c
stockfishdeveloper <davidandjava@mail.com>
2016-07-13 11:33:17 -0500

    Create Zobrist.h

---
+++ Zobrist.h
@@ -0,0 +1,11 @@
+#ifndef ZOBRIST_H_
+#define ZOBRIST_H_
+#include <cstdlib>
+#include <ctime>
+#include "Bitboard.h"
+#include "Search.h"
+extern Bitboard zobrist[64][12][2];
+extern void Init_Zobrist();
+extern Bitboard rand64();
+Bitboard Get_Current_Hash_Key();
+#endif
