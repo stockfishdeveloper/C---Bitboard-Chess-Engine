27140d514b6d63cc0951f1d5181fee2617af8f60
stockfishdeveloper <davidandjava@mail.com>
2016-07-13 11:30:38 -0500

    Restructure TT
    Put some of the declarations in Zobrist.h

--- TransTable.h
+++ TransTable.h
@@ -1,6 +1,25 @@
 #ifndef TRANS_TABLE_H_
 #define TRANS_TABLE_H_
 #include "Bitboard.h"
-void Init_Zobrist();
-extern Bitboard zobrist[64][6][2];
+#include "Search.h"
+enum NodeType { Alpha, Exact, Beta };
+class TTEntry
+{
+	public:
+	Bitboard key;
+	int depth;
+	int score;
+	Move best;
+	NodeType nodetype;
+};
+class TranspositionTable
+{
+	public:
+	TTEntry table[100000];
+	int count = 0;
+	TTEntry * probe(const Bitboard key);
+	void save(int depth, int score, Move best, NodeType n, Bitboard haskkey);
+	void clear();
+};
+extern TranspositionTable TT;
 #endif
