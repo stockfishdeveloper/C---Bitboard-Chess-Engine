51b67e1ee677dfdfbfc0401018e03675a6c5fa7d
stockfishdeveloper <davidandjava@mail.com>
2016-07-29 01:09:48 -0500

    Change TT size
    The TT was too large so Making it smaller and also making it a size of a power of 2

--- TransTable.cpp
+++ TransTable.cpp
@@ -5,7 +5,7 @@
 TranspositionTable TT;
 TTEntry * TranspositionTable::probe(const Bitboard key1)
 {
-	int index = key1 % 100000;
+	int index = key1 % 8388608;
 	if(TT.table[index].key == key1)
 	return &table[index];
 	else
@@ -13,18 +13,20 @@ TTEntry * TranspositionTable::probe(const Bitboard key1)
 }
 void TranspositionTable::save(int depth1, int score1, Move best1, NodeType n, Bitboard hashkey)
 {
-	int index = hashkey % 100000;
+	int index = hashkey % 8388608;
 	if(TT.table[index].depth < depth1)
 	{
 		TTEntry tt;
 		tt.depth = depth1;
 		tt.score = score1;
-		tt.best = best1;
+		tt.best.From = best1.From;
+		tt.best.To = best1.To;
 		tt.key = hashkey;
 		tt.nodetype = n;
 		TT.table[index] = tt;
 		TT.count++;
 	}
+	TT.table[index].visited++;
 }
 void TranspositionTable::clear()
 {
