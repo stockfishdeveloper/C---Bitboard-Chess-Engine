ea5b68d92928c7cb519714c830bbbeab7cb411dd
stockfishdeveloper <davidandjava@mail.com>
2016-08-09 14:07:38 -0500

    Refactor

--- Util.cpp
+++ Util.cpp
@@ -7,3 +7,27 @@ int lsb(const Bitboard& b)
 		return i;
 	}
 }
+Movetype Get_Move_Type(Move& m)
+{
+	if(m.Promotion)
+		return Promotion;
+	if(m.C != NONE)
+		return Capture;
+	else
+		return Normal;
+}
+const int Convert_Bitboard(const Bitboard& board)
+    {
+        if(board == 0)
+            return 0;
+        for(int i = 0; i < 64; i++)
+        {
+            if(board & GeneralBoard[i])
+                return i;
+        }
+        return 0;
+    }
+const Bitboard Unconvert_Int(const int& number)
+	{
+        return GeneralBoard[number];
+    }
