15775b230f5cc87b82dad4928be8e764df3cf3e4
stockfishdeveloper <davidandjava@mail.com>
2016-08-09 14:08:00 -0500

    Refactor

--- Util.h
+++ Util.h
@@ -1,5 +1,10 @@
 #ifndef UTIL_H
 #define UTIL_H
 #include "Bitboard.h"
+#include "Search.h"
 int lsb(const Bitboard& b);
+enum Movetype { Capture, Promotion, Normal };
+Movetype Get_Move_Type(Move& m);
+const int Convert_Bitboard(const Bitboard& board);
+const Bitboard Unconvert_Int(const int& number);
 #endif
