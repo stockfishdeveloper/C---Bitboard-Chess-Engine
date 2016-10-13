999b4d2683371216bae9156ac8abfd856a61ad13
stockfishdeveloper <davidandjava@mail.com>
2016-10-13 10:25:50 -0500

    Small update

--- Main.cpp
+++ Main.cpp
@@ -6,9 +6,8 @@ using namespace std;
 int main()
 {
     initmagicmoves();//Thanks, Pradu
-    cout << Engine_Info();
+    Engine_Info();
     cout << "\nA chess engine by David Cimbalista\nCopyright (c) 2015" << endl;
-    Init_Zobrist();
     CheckUci(); //Main command loop
     return 0;
 }
