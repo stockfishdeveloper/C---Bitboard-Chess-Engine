c9d24b541136a3cb246cbbc56f60534568b07961
stockfishdeveloper <davidandjava@mail.com>
2016-10-13 10:22:33 -0500

    Small optimizations

--- Bitboard.h
+++ Bitboard.h
@@ -5,7 +5,7 @@
 typedef unsigned long long int Bitboard;
 using namespace std;
 //This is an array containing one bit set in each bitboard corresponding to the index of the array; the square A1 is the first (0th) element
-extern Bitboard GeneralBoard[64];
+extern const Bitboard GeneralBoard[64];
 
 extern Bitboard Eigth_Rank_White;//For masking out pawn pushes to the eagth rank for each side--white and black
 extern Bitboard Eigth_Rank_Black;//See last comment
@@ -13,12 +13,12 @@ extern Bitboard Seventh_Rank_White;//For masking out pawn pushes to the eagth ra
 extern Bitboard Seventh_Rank_Black;//See last comment
 extern Bitboard CurrZobkey;
 //Contains the squares that a knight on a given index can move to. For example, the 0th element has the bits of b3 and c2 set
-extern Bitboard Knight_Lookup_Table[64];
+extern const Bitboard Knight_Lookup_Table[64];
 
 
 
 //This one is the same as the Knight_Lookup_Table except that this one is for kings
-extern Bitboard King_Lookup_Table[64];
+extern const Bitboard King_Lookup_Table[64];
 
 
 extern Bitboard H_Pawn_Mask;//For masking out A and H pawns so that they cannot capture off of the end of the board
