4b561e72a4270a49c1cd84f0345d3a94b7fde7e1
stockfishdeveloper <davidandjava@mail.com>
2016-11-10 11:26:16 -0600

    Vastly speed up lsb()
    Basically it uses a lookup table to speed it up

--- Util.h
+++ Util.h
@@ -2,11 +2,22 @@
 #define UTIL_H
 #include "Bitboard.h"
 #include "Search.h"
-short lsb(Bitboard b);
-int test_lsb(Bitboard b);
+int lsb(Bitboard b);
 enum Movetype { Capture, Promotion, Normal };
 Movetype Get_Move_Type(Move& m);
-const short Convert_Bitboard(const Bitboard& board);
-const Bitboard Unconvert_Int(const int& number);
+Bitboard Unconvert_Int(const int& number);
 void Benchmark();
+const char LSB_64_table[154] =
+{
+#define __ 0
+   22,__,__,__,30,__,__,38,18,__, 16,15,17,__,46, 9,19, 8, 7,10,
+   0, 63, 1,56,55,57, 2,11,__,58, __,__,20,__, 3,__,__,59,__,__,
+   __,__,__,12,__,__,__,__,__,__, 4,__,__,60,__,__,__,__,__,__,
+   __,__,__,__,21,__,__,__,29,__, __,37,__,__,__,13,__,__,45,__,
+   __,__, 5,__,__,61,__,__,__,53, __,__,__,__,__,__,__,__,__,__,
+   28,__,__,36,__,__,__,__,__,__, 44,__,__,__,__,__,27,__,__,35,
+   __,52,__,__,26,__,43,34,25,23, 24,33,31,32,42,39,40,51,41,14,
+   __,49,47,48,__,50, 6,__,__,62, __,__,__,54
+#undef __
+};
 #endif
