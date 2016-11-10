a25e2dd93ae50a80d5b78c21504ebe361bfe7d48
stockfishdeveloper <davidandjava@mail.com>
2016-11-10 11:13:31 -0600

    Adjust headers

--- Endgame.h
+++ Endgame.h
@@ -1,5 +1,5 @@
 #ifndef ENDGAME_H_INCLUDED
 #define ENDGAME_H_INCLUDED
-const int Drive_To_Corner(bool Losing_King);
-const int Dist_Betw(Bitboard sq1, Bitboard sq2);
+int Drive_To_Corner(Bitboard Losing_King);
+int Dist_Betw(Bitboard sq1, Bitboard sq2);
 #endif
