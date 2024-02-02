d5b4b94572f020ae04fb4e2881dbd03945c4f95a
stockfishdeveloper <cimbalistad@gmail.com>
2024-02-02 13:47:19 -0700

    Fix issue with perft depth 1

--- Perft.cpp
+++ Perft.cpp
@@ -1,52 +1,17 @@
 #include "Perft.h"
 #include "Search.h"
 int Root_Perft(int depth) {
-	if (depth == 0) return 1;
+	if (depth == 0) return 0;
+
+	cout << "\n";
+
 	Bitboard nodes = 0;
 	pos.Current_Turn ? Generate_White_Moves(false, pos) : Generate_Black_Moves(false, pos);
 	for (int i = 0; i < pos.numlegalmoves; i++) {
 		pos.Make_Move(pos.LegalMoves[i]);
-		for (int h = 0; h < 64; h++) {
-			if (GeneralBoard[h] & pos.LegalMoves[i].From) {
-				cout << PlayerMoves[h];
-			}
-		}
-		for (int h = 0; h < 64; h++) {
-			if (GeneralBoard[h] & pos.LegalMoves[i].To) {
-				cout << PlayerMoves[h];
-				if (pos.LegalMoves[i].Promotion) {
-					switch (pos.LegalMoves[i].PromotionType) {
-					case WN:
-						cout << "n";
-						break;
-					case WB:
-						cout << "b";
-						break;
-					case WR:
-						cout << "r";
-						break;
-					case WQ:
-						cout << "q";
-						break;
-					case BN:
-						cout << "n";
-						break;
-					case BB:
-						cout << "b";
-						break;
-					case BR:
-						cout << "r";
-						break;
-					case BQ:
-						cout << "q";
-						break;
-					default:
-						break;
-					}
-				}
-				cout << ": ";
-			}
-		}
+				
+		Print_Move_List(i);
+
 		int f = nodes;
 		nodes += Perft(pos, depth - 1);
 		cout << nodes - f << endl;
@@ -61,6 +26,9 @@ int Perft(Position& posit, int depth) {
 		position.Current_Turn ? Generate_White_Moves(false, position) : Generate_Black_Moves(false, position);
 		return position.numlegalmoves;
 	}
+
+	if (depth == 0) return 1;
+
 	Position position(posit);
 	Bitboard nodes = 0;
 	position.Current_Turn ? Generate_White_Moves(false, position) : Generate_Black_Moves(false, position);
@@ -70,4 +38,49 @@ int Perft(Position& posit, int depth) {
 		position.Undo_Move(position.LegalMoves[i]);
 	}
 	return nodes;
+}
+
+void Print_Move_List(int i) {
+	for (int h = 0; h < 64; h++) {
+		if (GeneralBoard[h] & pos.LegalMoves[i].From) {
+			cout << PlayerMoves[h];
+		}
+	}
+
+	for (int h = 0; h < 64; h++) {
+		if (GeneralBoard[h] & pos.LegalMoves[i].To) {
+			cout << PlayerMoves[h];
+			if (pos.LegalMoves[i].Promotion) {
+				switch (pos.LegalMoves[i].PromotionType) {
+				case WN:
+					cout << "n";
+					break;
+				case WB:
+					cout << "b";
+					break;
+				case WR:
+					cout << "r";
+					break;
+				case WQ:
+					cout << "q";
+					break;
+				case BN:
+					cout << "n";
+					break;
+				case BB:
+					cout << "b";
+					break;
+				case BR:
+					cout << "r";
+					break;
+				case BQ:
+					cout << "q";
+					break;
+				default:
+					break;
+				}
+			}
+			cout << ": ";
+		}
+	}
 }
--- Perft.h
+++ Perft.h
@@ -2,6 +2,7 @@
 #define PERFT_H_INCLUDED
 #include "Position.h"
 
+void Print_Move_List(int i);
 int Root_Perft(int depth);
 int Perft(Position& posit, int depth);
 #endif
